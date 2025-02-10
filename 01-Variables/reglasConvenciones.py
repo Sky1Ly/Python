9585773b0ee08731a816a960bfebd6dfaa41307e
Eider Agudelo <eider841agudelo@hotmail.com>
2025-02-09 22:37:34 -0300

    Se guarda la convención de variables

---
+++ 01-Variables/reglasConvenciones.py
@@ -0,0 +1,22 @@
+# Reglas y convenciones en nombres y variables
+
+
+#Ejemplos de reglas estrictas
+nombreUsuario="Juan Perez"
+# Forma Incorrecta: 1nombreUsuario="Juan Perez"
+
+# No se pueden usar palabras reservadas
+#class = "Mi Clase"
+
+# Sensibles a mayusculas y minusculas
+nombre= "Juan"
+Nombre="Carlos"
+print(nombre)
+print(Nombre)
+
+# Buenas Practicas con Snake Case
+nombre_usuario = "Ricardo Esparza"
+
+# prefijos y sufijos
+es_casado = True
+nombre_txt = "archivo.txt"
---
+++ 01-Variables/variables.py
@@ -0,0 +1,27 @@
+# Variables en Python
+
+# Declaracion e inicializacion de vairables
+edad = 28
+altura = 1.72
+pais = 'Colombia'
+
+# Acceder a las variables
+print("Valores iniciales")
+print('Edad:', edad)
+print("Altura:",altura)
+print("Pais:",pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.75
+pais = "Chile"
+
+# Acceder a las variables
+print("Valores Modificados")
+print('Edad:', edad)
+print("Altura:",altura)
+print("Pais:",pais)
+
+# En Python el tipado es dinámico
+edad = "treinta"
+print('Edad:', edad)
